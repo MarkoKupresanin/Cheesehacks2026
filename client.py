61dec73d69e4276ae7da0574897fa82c0cf3d949
Marko Kupresanin <kupresaninmarko2@gmail.com>
2026-02-28 19:12:22 -0600

    fix client

--- client.py
+++ client.py
@@ -2,23 +2,26 @@ from reedsolo import RSCodec, ReedSolomonError
 import hashlib
 import numpy
 import os
+load_dotenv()
+
 
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 rsc = RSCodec(10)
-
+#aesgcm = AESGCM(os.getenv("AES_KEY"))
 
 def encrypt_frame(frame):
     frame_bytes = frame.tobytes()
-    parity_bits = rsc.encode(frame_bytes)
 
     # Hash with SHA-256
-    frame_SHA = hashlib.sha256()
-    frame_SHA.update(frame_bytes)
+    frame_SHA = hashlib.sha256(frame_bytes).digest()
 
 	# Generate a 256-bit key (32 bytes), AES-256
 	key = AESGCM.generate_key(bit_length=256)
 	aesgcm = AESGCM(key)
     nonce = os.urandom(12)
-    frame_AES = aesgcm.encrypt(nonce, frame_bytes)
 
-    return (nonce+frame_AES, frame_SHA.digest(), parity_bits)
+    encrypted_frame = aesgcm.encrypt(nonce, frame_bytes + frame_SHA, None)
+
+    combined_payload = rsc.encode(nonce + encrypted_frame)
+
+    return combined_payload
